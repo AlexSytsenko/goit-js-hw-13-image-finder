83943e06b481cf44a08d715cd3ae7e113037c83d
AlexSytsenko <sytsenko@gmail.com>
2021-02-17 09:45:50 +0200

    resolved mistakes with coming response

--- src/index.js
+++ src/index.js
@@ -9,19 +9,21 @@ const refs = {
 
 const apiKey = "20059079-3862fc9514b48e56c5e47271f";
 const query = "cat";
-const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${query}&page=1&per_page=12&key=${apiKey}`;
+const url = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&page=1&per_page=12`;
 
-
+// const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${query}&page=1&per_page=3&key=${apiKey}`;
 
 fetch(url)
     .then(response => response.json())
-    .then(hits => createMarkup(hits))
+    .then(({ hits }) => createMarkup(hits))
     .catch(error => console.log(error));
 
 
 
 
 function createMarkup(arr) {
+
+    console.log(arr);
     const markup = templateImg(arr);
 
     refs.gallery.insertAdjacentHTML('beforeend', markup);
