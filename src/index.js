334e8697c93ff7d11e068403006174ceb660e399
AlexSytsenko <sytsenko@gmail.com>
2021-02-18 15:31:57 +0200

    rewrote code for button from function to class

--- src/index.js
+++ src/index.js
@@ -1,34 +1,29 @@
 import './styles.scss';
 import photoApi from './js/photo-api';
+import refs from './js/refs';
+import LoadMoreBtn from './js/components/load-more-btn';
 import templateImg from './templates/template-img.hbs';
 import 'material-design-icons/iconfont/material-icons.css';
 
 
-
-const refs = {
-    searchForm: document.getElementById('search-form'),
-    gallery: document.querySelector('.gallery'),
-    loadMoreBtn: document.querySelector('[data-action="load-more"]'),
-    loadMoreBtnLabel: document.querySelector('button > .label'),
-    loadMoreBtnSpinner: document.querySelector('button > .spinner'),
-};
+const loadMoreBtn = new LoadMoreBtn({
+    selector: '[data-action="load-more"]',
+    hidden: true
+});
 
 
 refs.searchForm.addEventListener('submit', searchFormSubmitHandler);
-refs.loadMoreBtn.addEventListener('click', loadMoreBtnHandler);
+loadMoreBtn.refs.button.addEventListener('click', loadMoreBtnHandler);
 
 
 function searchFormSubmitHandler(event) {
     event.preventDefault();
-    const form = event.currentTarget;
 
+    const form = event.currentTarget;
     photoApi.query = form.elements.query.value;
 
-
     photoApi.resetPage();
-    clearMarkup();
-
-
+    clearPhotosMarkup();
     fetchPhotos();
 
     // console.log(event.currentTarget);
@@ -37,48 +32,37 @@ function searchFormSubmitHandler(event) {
 }
 
 function fetchPhotos() {
-
-    refs.loadMoreBtn.disabled = true;
-    refs.loadMoreBtnLabel.textContent = 'Loading...';
-    refs.loadMoreBtnSpinner.classList.remove('is-hidden');
+    loadMoreBtn.disable();
     
     photoApi
         .fetchPhotos()
         .then(hits => {
-            updateMarkup(hits);
-            refs.loadMoreBtn.disabled = false;
-            refs.loadMoreBtnLabel.textContent = 'Load more...';
-            refs.loadMoreBtnSpinner.classList.add('is-hidden');
+            updatePhotosMarkup(hits);
+            loadMoreBtn.show();
+            loadMoreBtn.enable();
 
             window.scrollTo({
                 top: document.documentElement.offsetHeight,
                 behavior: 'smooth'
             });
             
-        })
-        .finally(() => {
-            // refs.spinner.classList.add('is-hidden');
         });
-    
-    
 }
 
 
 
-function updateMarkup(array) {
+function updatePhotosMarkup(array) {
     const markup = templateImg(array);
 
     refs.gallery.insertAdjacentHTML('beforeend', markup);
     
 }
 
-function clearMarkup() {
+function clearPhotosMarkup() {
     refs.gallery.innerHTML = '';
 }
 
 function loadMoreBtnHandler() {
-
-    // refs.spinner.classList.remove('is-hidden');
     
     photoApi.incrementPage();
     fetchPhotos();
