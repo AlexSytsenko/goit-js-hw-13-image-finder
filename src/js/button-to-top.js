f9289aa01c7653c2f2406644368b14fe8202feb5
AlexSytsenko <sytsenko@gmail.com>
2021-02-27 20:42:50 +0200

    add a throttle function for scroll

--- src/js/button-to-top.js
+++ src/js/button-to-top.js
@@ -1,17 +1,21 @@
 import refs from './refs';
 
+import { throttle } from 'throttle-debounce';
+
+
 
 let rootElement = document.documentElement;
 
-function handleScroll() {
+const handleScroll = throttle(350, () => {
+  console.log('scroll');
     
-    if ((rootElement.scrollTop - document.documentElement.clientHeight) > 0) {
-        refs.scrollToTopBtn.classList.remove('is-hidden');
+  if ((rootElement.scrollTop - document.documentElement.clientHeight) > 0) {
+    refs.scrollToTopBtn.classList.remove('is-hidden');
       
   } else {
     refs.scrollToTopBtn.classList.add('is-hidden');
   }
-}
+});
 
 function scrollToTop() {
   rootElement.scrollTo({
